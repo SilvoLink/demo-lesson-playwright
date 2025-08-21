4066f03cda118dcfb34c4dcdfd6ae1e9b6406571
silvo <silvolink@gmail.com>
2025-08-21 13:58:56 +0300

    Initial commit

--- tests/pages/order-page.ts
+++ tests/pages/order-page.ts
@@ -4,9 +4,23 @@ export class OrderPage {
   readonly page: Page
   readonly statusButton: Locator
   // add more locators here
+  readonly logoutButton: Locator
+  readonly orderCreationButton: Locator
+  readonly usernameInput: Locator
+  readonly phoneInput: Locator
+  readonly commentInput: Locator
+  readonly orderCreatedOkButton: Locator
+  readonly phoneInputError: Locator
 
   constructor(page: Page) {
     this.page = page
     this.statusButton = page.getByTestId('openStatusPopup-button')
+    this.logoutButton = page.getByTestId('logout-button')
+    this.orderCreationButton = page.getByTestId('createOrder-button')
+    this.usernameInput = page.getByTestId('username-input')
+    this.phoneInput = page.getByTestId('phone-input')
+    this.commentInput = page.getByTestId('comment-input')
+    this.orderCreatedOkButton = page.getByTestId('orderSuccessfullyCreated-popup-ok-button')
+    this.phoneInputError = page.getByTestId('phone-input-error')
   }
 }
